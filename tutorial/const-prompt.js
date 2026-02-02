0cb237240d653c16e81f7d353a120dcb972185de
francimezze@gmail.com <mezze@debian.mezze>
2026-02-03 00:47:04 +0100

    modifiche e vari testo alla repo

---
+++ tutorial/const-prompt.js
@@ -0,0 +1,13 @@
+// 1. Importa il modulo e inizializzalo
+const prompt = require("prompt-sync")();
+
+// 2. Utilizzalo per chiedere un input
+const nome = prompt("Come ti chiami? ");
+
+console.log("Ciao " + nome + "!");
+
+// Esempio con numeri (ricorda: l'input è sempre una stringa, serve il cast)
+const anni = prompt("Quanti anni hai? ");
+const anniNumerico = parseInt(anni);
+
+console.log("L'anno prossimo ne avrai " + (anniNumerico + 1));
