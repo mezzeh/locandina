9cfe01a649dc509c291198da8064a12341a97f3c
francimezze@gmail.com <mezze@debian.mezze>
2026-02-03 15:29:23 +0100

    lklkl

--- esercizi/es/es3.js
+++ esercizi/es/es3.js
@@ -1,4 +1,3 @@
-
 let n = 0;
 /* console.log("il valore f(n) di ",n," è ",fibonacciNsimo(n))
 function fibonacciNsimo (n)
@@ -8,7 +7,7 @@ function fibonacciNsimo (n)
             console.log("+0 ")
         return 0;
     }
-        
+
     if(n ==1)
     {
         console.log("+ 1")
@@ -16,30 +15,27 @@ function fibonacciNsimo (n)
     }
     return fibonacciNsimo(n-1)+fibonacciNsimo(n-2)
 }
- */
-n = 6
-arr = fibonacciPrimiN(n)
+ */ fgfgfg;
+n = 6;
+arr = fibonacciPrimiN(n);
 // una funzione che dato n ritorna i primi n numeri della successione: n = 7 =  0,1,1,2,3,5,8
 //n = 3 0,1,1function fibonacciNsimo (n)
-function fibonacciPrimiN(n)
-{
-    if (n == 0) {
-      
-        return [1];
-    }
+function fibonacciPrimiN(n) {
+  if (n == 0) {
+    return [1];
+  }
 
-    if (n == 1) {
-       
-        return [1,1];
-    }
-    
-   //return fibonacciNsimo(n-1) + fibonacciNsimo(n-2)
-    let seq = fibonacciPrimiN(n-1)
-    let nesimo = seq[seq.length-1] + seq[seq.length-2]
-    seq.push(nesimo)
-    return seq;
+  if (n == 1) {
+    return [1, 1];
+  }
 
-    //ora che ho il risultato devo includerlo in un array
+  //return fibonacciNsimo(n-1) + fibonacciNsimo(n-2)
+  let seq = fibonacciPrimiN(n - 1);
+  let nesimo = seq[seq.length - 1] + seq[seq.length - 2];
+  seq.push(nesimo);
+  return seq;
+
+  //ora che ho il risultato devo includerlo in un array
 }
 
-console.log("l'f di n è : ",arr)
+console.log("l'f di n è : ", arr);
