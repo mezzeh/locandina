fa3447db01938fe9cf13ec4072fb1f80eaea8318
francimezze@gmail.com <mezze@debian.mezze>
2026-02-04 08:18:26 +0100

    Update ricercaMassimo.js

--- algoritmi/ricercaMassimo.js
+++ algoritmi/ricercaMassimo.js
@@ -5,4 +5,15 @@ for (let i = 0; i < 10; i++) {
 }
 
 console.log(`Array: ${arr}`);
-
+let max = searchMax(arr);
+console.log(
+  `l'elemento massimo dell'array ${arr} è ${arr[max]} in posizione ${max}`,
+);
+//ricerca massimo
+function searchMax(array) {
+  let max = 0;
+  for (let i = 0; i <= array.length; i++) {
+    if (array[max] < array[i]) max = i;
+  }
+  return max;
+}
