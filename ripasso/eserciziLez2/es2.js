a3624e2d6d21c46a207ebade566d0640a811ab06
francimezze@gmail.com <mezze@debian.mezze>
2026-02-05 18:41:03 +0100

    update es2.js

--- ripasso/eserciziLez2/es2.js
+++ ripasso/eserciziLez2/es2.js
@@ -2,6 +2,15 @@
 
 const prompt = require("prompt-sync")();
 
-let C = Number(prompt("inserisci il numero: "));
+// //let C = Number(prompt("inserisci il numero: "));
 
-console.log(`la temperatura è : ${C * 1.8 + 32}`);
+// console.log(`la temperatura è : ${C * 1.8 + 32}`);
+
+let seconds = 5600; //prompt("inserisci i secondi vhe vuoi trasformare: ");
+let time = {
+  ore: Math.floor(seconds / 3600),
+  minuti: Math.floor((seconds % 3600) / 60),
+  secondi: (seconds % 3600) % 60, // calcolo che ottenendo i miinuti rimanenti togliendo le ore, posso ottenere i secondi rimanenti togliendo i minuti
+};
+
+console.log(`In ${seconds} secondi ci sono: `, time);
