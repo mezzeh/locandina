f18d8b8b2c2b9c4a7a23d6c862e1bf4f38ec1a67
francimezze@gmail.com <mezze@debian.mezze>
2026-02-15 19:37:56 +0100

    updatw

--- parziale4/ripassoEsercitazione/esercizi.js
+++ parziale4/ripassoEsercitazione/esercizi.js
@@ -9,3 +9,44 @@
  La funzione deve restituire il nuovo multi-insieme,
  e il numero di elementi cancellati.
  */
+// let p = x => x > 2
+// let S = { "banane": 2, "PEre": 4 }
+// console.log()
+// console.log(filteredset(S,p))
+// function filteredset(S,p)
+// {
+//   let newa = {}
+//   for (let el in S)
+//   {
+//     if (p(S[el]))
+//       newa[el] = S[el]
+//   }
+//   return [newa , Object.keys(S).length- Object.keys(newa).length ]
+// }
+
+
+/*Esercizio 2 - ksumlimit
+Si scriva una funzione JavaScript
+ksumlimit(p, k, n1, n2, n3, ...).
+La funzione restituisce un array con i primi numeri,
+incontrati scorrendo da sinistra verso destra,
+strettamente maggiori di p
+la cui somma totale non supera k.
+ESEMPI:
+ksumlimit(3, 20, 5, 10, 4, 8, 1, 7) --> [5, 10, 4] */
+console.log(ksumlimit(3, 20, 5, 10, 4, 8, 1, 7))
+function ksumlimit(p, k, ...n)
+{
+  let newa = n.filter((x => (x > p)))
+  let cont = []
+  newa = n.reduce((acc, curr) =>{
+    if ((curr + acc) < k)
+    {
+      cont.push(curr)
+      acc += curr
+        }
+    console.log(acc)
+    return acc
+  }, 0)
+  return [newa,cont]
+}
