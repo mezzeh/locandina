c1e9e91ec9a177b37f3619db21cb5d951757415f
francimezze@gmail.com <mezze@debian.mezze>
2026-03-04 11:18:14 +0100

    cmmit

--- uni_lab_js/parziali/parziale5/esercitazione/primo.js
+++ uni_lab_js/parziali/parziale5/esercitazione/primo.js
@@ -1,83 +1,4 @@
 import fs from "fs"
-class GradeBook
-{
-   #grades  = [];
-  constructor(tc) {
-    this.TotalCredits = tc;
-   }
-  get average()
-  {
-    return Math.floor(this.#grades.reduce((acc,curr) => acc+curr.voto,0)/this.#grades.length)
-  }
-  get credits()
-  {
-    return this.#grades.reduce((acc, curr) => acc + curr.crediti,0)
-  }
-  get missingCredits()
-  {
-
-    if (this.TotalCredits == 180)new MissingCreditsError("prova");
-    return this.TotalCredits- this.credits;
-  }
-  get StartGrade()
-  {
-    return   Math.floor((this.average * 110) / 30)
-  }
-  registra(...v)
-  {
-
-    for (let graded of v)
-      if(!this.#grades.includes(graded) )
-        if(graded instanceof grade)
-          this.#grades.push(graded)
-          else
-            throw new TypeError("La variable must be a class grade")
-      else
-        throw new DuplicatedGradeError("Non immettere esami duplicati")
-
-  }
-  get obj()
-  {
-    return {grades: this.#grades, credits: this.credits, average: this.average, missingCredits: this.missingCredits, StartGrade: this.StartGrade}
-  }
-  toString()
-  {
-    return JSON.stringify(this.obj)
-  }
-  exportJSON(f)
-  {
-
-    fs.writeFileSync(f, ga.toString())
-  }
-  static fromJSON(f)
-  {
-    let data = fs.readFileSync(f)
-    data = JSONparse(data)
-    // come metto in ordine gli oggetti da passare? un for che inseirsce ?
-    //ora io ho l'oggetto
-    let arr = data.grades
-    let newb = new GradeBook(data.TotalCredits)
-    newb.registra(...arr.map((x) => x = new grade(x.nome,x.voto, x.credit,x.data,x.lode)))
-
-  }
-
-}
-class MasterGradebook extends GradeBook
-{
-  constructor()
-  {
-    super();
-    this.tc = 120;
-
-  }
-}
-class BachelorGradebook extends GradeBook
-{
-  constructor()
-  {
-    super();
-  }
-}
 class grade
 {
   #nome
@@ -94,10 +15,13 @@ class grade
        this.#crediti = Number(c);
     this.#data = new Date(d) // e("19 marzo 2021"
     if (!(v >= 18 && v <= 30))
-      throw new InvalidValueError("IL VOTO DEVE ESSERE COMPRESO TRA 18 E 30 ")
+    {
+      console.log("voto sbagliato : ",v)
+      throw new InvalidValueError("IL VOTO DEVE ESSERE COMPRESO TRA 18 E 30 ",v)
+      }
       else this.#voto = Number(v);
     if (l && v != 30)
-      throw new InvalidValueError("NON PUOI ASSEGNARE LA LODE AD UN VOTO DIVERSO DA 30")
+      throw new InvalidValueError("NON PUOI ASSEGNARE LA LODE AD UN VOTO DIVERSO DA 30",v)
       else
         this.#lode = Boolean(l)
       }
@@ -122,11 +46,16 @@ class grade
       }
       toString()
       {
+
         return JSON.stringify(this.obj)
       }
       get obj()
       {
         return {nome : this.#nome , crediti : this.#crediti , data : this.#data , voto : this.#voto , lode : this.#lode}
+      }
+
+      toJSON(){ // <-- invocato da JSON.stringify
+          return this.obj
       }
   get crediti()
   {
@@ -136,6 +65,88 @@ class grade
   get voto()
   {
     return this.#voto
+  }
+}
+
+class GradeBook {
+  #grades = [];
+  constructor(tc) {
+    this.TotalCredits = tc;
+  }
+  get average() {
+    return Math.floor(this.#grades.reduce((acc, curr) => acc + curr.voto, 0) / this.#grades.length)
+  }
+  get credits() {
+    return this.#grades.reduce((acc, curr) => acc + curr.crediti, 0)
+  }
+  get missingCredits() {
+
+    if (this.TotalCredits == 180) new MissingCreditsError("prova");
+    return this.TotalCredits - this.credits;
+  }
+  get StartGrade() {
+    return Math.floor((this.average * 110) / 30)
+  }
+  registra(...grades) {
+
+    // for (let graded of v)
+    //   if (!this.#grades.includes(graded))
+    //     if (graded instanceof grade)
+    //       this.#grades.push(graded)
+    //     else
+    //       throw new TypeError("La variable must be a class grade")
+    //   else
+    //     throw new DuplicatedGradeError("Non immettere esami duplicati")
+
+
+             for (let grad of grades)
+                 if (grad instanceof grade){
+                     this.#grades.push(grad)
+                 }
+
+  }
+  get obj() {
+    return { grades: this.#grades, credits: this.credits, average: this.average, missingCredits: this.missingCredits, StartGrade: this.StartGrade }
+  }
+  get grades()
+  {
+    return this.#grades
+  }
+  toString() {
+    // nell'esporazione i grades non vengono correttamente esportati:avendone due non li visualizzo.
+    return JSON.stringify({grades : this.#grades,TotalCredits: this.TotalCredits})
+  }
+  exportJSON(f){
+        let s = this.toString()
+        fs.writeFileSync(f,s)
+    }
+
+   static fromJSON(f) {
+    let s = fs.readFileSync(f)
+    let ogg = JSON.parse(s)
+    let newb = new GradeBook(ogg.TotalCredits)
+     let arr = ogg.grades
+
+     newb.registra(...arr.map(x => new grade(x.nome, x.crediti, x.data, x.voto, x.lode)))
+     console.log(newb.toString())
+    return newb
+
+  }
+}
+class MasterGradebook extends GradeBook
+{
+  constructor()
+  {
+    super();
+    this.tc = 120;
+
+  }
+}
+class BachelorGradebook extends GradeBook
+{
+  constructor()
+  {
+    super();
   }
 }
 
@@ -158,9 +169,10 @@ class DuplicatedGradeError extends Error
   }
 class InvalidValueError extends Error
 {
-  constructor(txt)
+  constructor(txt,v)
   {
     super(txt);
+    this.creditosbagliato = v
   }
 };
 class MissingCreditsError extends Error {
@@ -177,14 +189,16 @@ try {
   var libretto = new GradeBook( 180)
   libretto.registra(fdi);
   libretto.registra(g);
-
   libretto.exportJSON('testo.txt')
-  GradeBook.fromJSON('testo.txt')
+  console.log("oggetto creato dal file:",GradeBook.fromJSON('testo.txt'))
+
 }
 catch (e)
 {
-  console.log(e instanceof TypeError); // true
+  console.log(e instanceof InvalidValueError);
+  console.log(e.creditosbagliato)
   console.log(e.message); // "Hello"
   console.log(e.name); // "TypeError"
   console.log(e.stack); // Stack of the error
 }
+export {GradeBook,MasterGradebook,grade,InvalidCreditsError,MissingCreditsError}
---
+++ uni_lab_js/parziali/parziale5/esercitazione/rexexp.js
@@ -0,0 +1,4 @@
+import * as uni from './primo.js';
+
+let libricino = new uni.GradeBook(180)
+console.log(libricino)
