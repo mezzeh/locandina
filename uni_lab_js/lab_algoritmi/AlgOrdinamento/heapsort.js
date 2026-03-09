a2eabfaa8d2b2386ea77b54d58b99f6efcc11168
mezze-win11 <francimezze@gmail.com>
2026-03-09 12:15:58 +0100

    mm

---
+++ uni_lab_js/lab_algoritmi/AlgOrdinamento/heapsort.js
@@ -0,0 +1,29 @@
+/**
+ * Sinistro: Left(i)=2i+1 
+Destro: Right(i)=2i+2 
+Padre: Parent(i)=⌊(i−1)/2⌋ 
+ */
+let arr =  [15,3,4,8,20,30]
+buildHeap(arr)
+console.log(arr)
+function buildHeap(A)
+{
+    //inizia con un for.
+    let len = A.length;
+    //da quanto parte? parte da uno perche dobbiamo andare ad analizzare i padri non i nodi in se
+    for(let i = 1; i < A.length ;i++)
+    {   
+        let p = Math.floor((i-1)/2)
+        let j = i; // per il ciclo while 
+        console.log(A,i,p)
+        while(p >=0 && A[j] > A[p]) // se il padre è 0, è la radice, lui continua cioe, entra se non è corretto quindi se deve buildare.
+        {
+            console.log("dentro al while: ",i,j,p)
+            let temp = A[j] 
+            A[j] = A[p]
+            A[p] = temp
+            j = p // riparte automaticamente dal membro scambiato per rieffettuare il controllo su il padre e stabilire 
+            p = Math.floor((j-1)/2)// troviamo il padre di j !!
+        } 
+    }
+}
