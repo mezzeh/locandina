75f3a9b6a73b01eb031bb0517ca706830c5fd3e1
francimezze@gmail.com <mezze@debian.mezze>
2026-03-18 15:33:10 +0100

    fdsafd

--- uni_lab_js/lab_algoritmi/prDin/fiboric.js
+++ uni_lab_js/lab_algoritmi/prDin/fiboric.js
@@ -1,6 +1,40 @@
-var F = [1];
-
-function fib(R)
+console.log(fibric(5))
+function fibric(n)
 {
-conosle.log(F)
+  let F = [];
+  for (let i = 0; i <= n; i++)
+  {
+    F[i] = -1;
+
+    }
+  return [fib(n,F),F]
+}
+function fib(n,F)
+{
+
+  if (n == 0 || n == 1)
+  {
+    F[n] = n
+    return n
+    }
+  if (F[n] != -1) return F[n] // perche non funziona? perche entra qua fa il confronto ed esce, non fa la ricorsione.
+  else
+  {
+    F[n] = fib(n - 1, F) + fib(n - 2, F)
+
+    return F[n]
+    }
+
+}
+console.log(fibBU(5))
+function fibBU(n)
+{
+  let FI = []
+  FI[0] = 0;
+  FI[1] = 1;
+  for (let i = 2; i <= n; i++)
+  {
+    FI[i] = FI[i-1] + FI[i-2]
+    }
+  return FI
 }
