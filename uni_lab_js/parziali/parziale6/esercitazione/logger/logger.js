333fe2776d4085b9e5680f4c7e2c5070dee932a2
mezze-win11 <francimezze@gmail.com>
2026-04-04 11:20:57 +0200

    fdsas

--- uni_lab_js/parziali/parziale6/esercitazione/logger/logger.js
+++ uni_lab_js/parziali/parziale6/esercitazione/logger/logger.js
@@ -1,11 +1,12 @@
 "use strict";
+class UnknownLevel extends Error {
+}
 class Logger {
-    constructor(levels) {
-        /*   console.log( Levels['INFO']) */
-    }
     static log(level, message) {
-        console.log(level, message);
-        // ho capito ma come faccio a verificare che esso sia parte dei livelli consentiti? 
+        if (Levels[level] == undefined)
+            throw new UnknownLevel();
+        console.log(typeof Levels[level]);
+        console.log(Levels[level], message);
     }
 }
 var Levels;
@@ -14,7 +15,8 @@ var Levels;
     Levels[Levels["WARNING"] = 1] = "WARNING";
     Levels[Levels["ERROR"] = 2] = "ERROR";
 })(Levels || (Levels = {}));
-Logger.log(Levels['INFOf'], "CIAO"); // cosa succede se non è un levels?
+let log = new Logger();
+Logger.log(0, "CIAO");
 let colorName = Levels['INFO'];
 /* console.log(colorName) */
 function logLevel(level) {
