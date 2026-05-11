5458c2bb81cf5c8c094ca34ec7d9cc839b761422
francimezze@gmail.com <mezze@debian.mezze>
2026-05-11 22:18:21 +0200

    ds

--- uni_lab_js/parziali/parziale6/typescript.ts
+++ uni_lab_js/parziali/parziale6/typescript.ts
@@ -1,6 +1,6 @@
 // Source - https://stackoverflow.com/a/65852551
 // Posted by Aluan Haddad, modified by community. See post 'Timeline' for change history
-// Retrieved 2026-03-27, License - CC BY-SA 4.0
+// Retrieved 2026-03-27, License - CC BY-SA 4.0f
 
 import promptSync from 'prompt-sync';
 
@@ -9,18 +9,18 @@ const prompt = promptSync();
 const result  = prompt("inserisci il nomee");
 
 
-/* come si usa typescript????????? 
+/* come si usa typescript?????????
 
-RUNNARE FILE TS SENZA CONFIGURAZIONE TSCONFIG.JSON 
+RUNNARE FILE TS SENZA CONFIGURAZIONE TSCONFIG.JSON
 1) Compila il file ts: tsc file.ts
 2) esegui il file js creato: file.js
 
 
-SECONDO MODO: 
+SECONDO MODO:
 1) crea un progetto con tsc --init;
 2) compila l'intero progetto con tsc;
 3) esegui il file javascript
 c
 nb non so come non compilare l'intero progetto, suppongo che tu debba farlo e basta
-nb puoi comunque mandare online 
+nb puoi comunque mandare online
 */
